ef63fd0b098a39cfc87e63fd4e19c21d42766117
Daniel Coady <daniel.coady@outlook.com.au>
2017-11-01 05:21:29 +1100

    Added jumping and grounded check

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public float speed;
+    public float speed, jumpStrength;
+    public LayerMask groundLayer;
 
     private Rigidbody _rigidBody;
 
@@ -12,10 +13,13 @@ public class PlayerController : MonoBehaviour {
     void Start() {
         _rigidBody = GetComponent<Rigidbody>();
     }
-
+    
     // Update is called once per frame
     void FixedUpdate() {
         var movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         _rigidBody.AddForce(movement * speed);
+
+        if (Physics.Raycast(transform.position, Vector3.down, 1.0f, groundLayer.value) && Input.GetButtonDown("Jump"))
+            _rigidBody.AddForce(Vector3.up * jumpStrength, ForceMode.Impulse);
     }
 }
