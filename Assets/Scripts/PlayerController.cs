bab17fbcd785c765a8a2a30d056221a881ed52d4
Cat Flynn <mewtwo97@hotmail.com>
2017-10-31 19:20:44 +0000

    Make jumping more reliable

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,18 +8,34 @@ public class PlayerController : MonoBehaviour {
     public LayerMask groundLayer;
 
     private Rigidbody _rigidBody;
+    private bool _jump;
 
     // Use this for initialization
     void Start() {
         _rigidBody = GetComponent<Rigidbody>();
     }
-    
+
+    private void Update() {
+        if (!_jump) {
+            _jump = Input.GetButtonDown("Jump");
+        }
+    }
+
     // Update is called once per frame
     void FixedUpdate() {
         var movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         _rigidBody.AddForce(movement * speed);
 
-        if (Physics.Raycast(transform.position, Vector3.down, 1.0f, groundLayer.value) && Input.GetButtonDown("Jump"))
-            _rigidBody.AddForce(Vector3.up * jumpStrength, ForceMode.Impulse);
+        if (_jump) {
+            if (IsGrounded()) {
+                _rigidBody.AddForce(Vector3.up * jumpStrength, ForceMode.Impulse);
+            }
+
+            _jump = false;
+        }
+    }
+
+    private bool IsGrounded() {
+        return Physics.Raycast(transform.position + Vector3.up * 0.1f, Vector3.down, 0.2f, groundLayer.value);
     }
 }
