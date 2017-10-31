0a9a8335186019acec368dd2da95cb85de6d717e
Cat Flynn <mewtwo97@hotmail.com>
2017-10-31 16:58:25 +0000

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform focus;
+    public float lerpT;
+    public float distance;
+    public float angle;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        var rotation = Quaternion.Euler(angle, 0, 0);
+        var targetPosition = focus.position + rotation * new Vector3(0, 0, -distance);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, lerpT);
+        transform.LookAt(focus);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody _rigidBody;
+
+    // Use this for initialization
+    void Start() {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        var movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        _rigidBody.AddForce(movement * speed);
+    }
+}
