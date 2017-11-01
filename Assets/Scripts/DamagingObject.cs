a3a1e0d1060f2432aea191e1685a585a3baff541
Cat Flynn <mewtwo97@hotmail.com>
2017-11-01 04:45:45 +0000

    Add glass cabinet to damage player

---
+++ Assets/Scripts/DamagingObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagingObject : MonoBehaviour {
+
+    private Health _ownHealth;
+    private Health _playerHealth;
+
+	// Use this for initialization
+	void Start () {
+        _playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
+        _ownHealth = GetComponent<Health>();
+        _ownHealth.OnDamageTaken.AddListener(() => {
+            if (Random.value > 0.5) {
+                _playerHealth.TakeDamage(1);
+            }
+        });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
