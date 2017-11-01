ce8526e68013bd885803bf7da933e1256bc7c7c7
Cat Flynn <mewtwo97@hotmail.com>
2017-11-01 16:27:07 +0000

    Make pickup pickupable

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void OnTriggerEnter(Collider other) {
+        var player = other.gameObject.GetComponent<PlayerController>();
+        if (player == null)
+            return;
+
+        Destroy(gameObject);
+    }
 }
