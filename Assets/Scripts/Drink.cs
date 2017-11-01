31898fef5bfd178a33f4fda4fc1d2fa78a0b9053
Cat Flynn <mewtwo97@hotmail.com>
2017-11-01 21:44:31 +0000

    Rename Pickup->Drink

--- Assets/Scripts/Drink.cs
+++ Assets/Scripts/Drink.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pickup : MonoBehaviour {
+public class Drink : MonoBehaviour {
 
     // Use this for initialization
     void Start() {
