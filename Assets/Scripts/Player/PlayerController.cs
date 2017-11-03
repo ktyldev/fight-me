5b09d38507cc9fbbe3248411c28e8aa4ac8c2557
Cat Flynn <mewtwo97@hotmail.com>
2017-11-03 04:07:47 +0000

    Add animation triggers

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -10,14 +10,16 @@ public class PlayerController : MonoBehaviour {
     public float speed;
     public float drunkMotion;
     public float fallAnimTime;
+    public float getUpTime;
 
     private CharacterController _charController;
     private Health _health;
     private BloodAlcohol _bac;
     private PlayerCombat _combat;
     private Vector3 _movement = Vector3.zero;
-    private bool _airBorne;
     private float _fallTimer;
+    
+    private bool _fallenOver;
 
     void Start() {
         _health = GetComponent<Health>();
@@ -33,31 +35,48 @@ public class PlayerController : MonoBehaviour {
         }
 
         if (_charController.isGrounded) {
-            if (_airBorne) {
-                _airBorne = false;
+            if (_fallTimer != 0) {
                 if (_fallTimer > fallAnimTime) {
-                    print("trigger fall over");
+                    FallOver();
                 }
                 _fallTimer = 0;
             }
-            
-            var input = new Vector3(Input.GetAxis(GameInput.Horizontal), 0, Input.GetAxis(GameInput.Vertical));
-            var moveDir = Vector3.Lerp(input, _movement.normalized, _bac.Current * drunkMotion);
-            _movement = moveDir * speed;
 
-            if (_movement != Vector3.zero) {
-                // Look in the direction of movement, or direction last moved in if standing still
-                transform.LookAt(transform.position + (_movement.magnitude > 1 ? _movement : transform.forward));
-            }
-        } else  {
-            if (!_airBorne) {
-                _airBorne = true;
-            }
-
+            HandleInput();
+        } else {
             _fallTimer += Time.deltaTime;
         }
 
+        Move();
+    }
+
+    private void HandleInput() {
+        var input = new Vector3(Input.GetAxis(GameInput.Horizontal), 0, Input.GetAxis(GameInput.Vertical));
+        var moveDir = Vector3.Lerp(input, _movement.normalized, _bac.Current * drunkMotion);
+        _movement = moveDir * speed;
+
+        if (_movement != Vector3.zero) {
+            // Look in the direction of movement, or direction last moved in if standing still
+            transform.LookAt(transform.position + (_movement.magnitude > 1 ? _movement : transform.forward));
+        }
+    }
+
+    private void Move() {
         _movement.y += Physics.gravity.y * Time.deltaTime;
-        _charController.Move(_movement * speed * Time.deltaTime);
+        if (!_fallenOver) {
+            _charController.Move(_movement * speed * Time.deltaTime);
+        }
+    }
+
+    private void FallOver() {
+        _fallenOver = true;
+        print("trigger fall over");
+        StartCoroutine(GetUp());
+    }
+
+    private IEnumerator GetUp() {
+        yield return new WaitForSeconds(getUpTime);
+        print("trigger get up");
+        _fallenOver = false;
     }
 }
