f3f161caa8ab3d0fc56cabb61bae783ce54b3b19
Cat Flynn <mewtwo97@hotmail.com>
2017-11-03 03:18:20 +0000

    Falling animation trigger

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -9,12 +9,15 @@ public class PlayerController : MonoBehaviour {
 
     public float speed;
     public float drunkMotion;
+    public float fallAnimTime;
 
     private CharacterController _charController;
     private Health _health;
     private BloodAlcohol _bac;
     private PlayerCombat _combat;
     private Vector3 _movement = Vector3.zero;
+    private bool _airBorne;
+    private float _fallTimer;
 
     void Start() {
         _health = GetComponent<Health>();
@@ -30,6 +33,14 @@ public class PlayerController : MonoBehaviour {
         }
 
         if (_charController.isGrounded) {
+            if (_airBorne) {
+                _airBorne = false;
+                if (_fallTimer > fallAnimTime) {
+                    print("trigger fall over");
+                }
+                _fallTimer = 0;
+            }
+            
             var input = new Vector3(Input.GetAxis(GameInput.Horizontal), 0, Input.GetAxis(GameInput.Vertical));
             var moveDir = Vector3.Lerp(input, _movement.normalized, _bac.Current * drunkMotion);
             _movement = moveDir * speed;
@@ -38,6 +49,12 @@ public class PlayerController : MonoBehaviour {
                 // Look in the direction of movement, or direction last moved in if standing still
                 transform.LookAt(transform.position + (_movement.magnitude > 1 ? _movement : transform.forward));
             }
+        } else  {
+            if (!_airBorne) {
+                _airBorne = true;
+            }
+
+            _fallTimer += Time.deltaTime;
         }
 
         _movement.y += Physics.gravity.y * Time.deltaTime;
