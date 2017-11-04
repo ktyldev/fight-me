10b0ca8dac05654bbaf8c940b9864925069fa157
Cat Flynn <mewtwo97@hotmail.com>
2017-11-04 22:09:47 +0000

    Hide roof when player enters house

---
+++ Assets/Scripts/Roof.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roof : MonoBehaviour {
+
+    private GameObject _player;
+    private MeshRenderer _roof;
+    
+    void Start() {
+        _player = GameObject.FindGameObjectWithTag(GameTags.Player);
+        _roof = transform.GetComponentInChildren<MeshRenderer>();
+    }
+    
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject != _player)
+            return;
+        
+        _roof.gameObject.SetActive(false);
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (other.gameObject != _player)
+            return;
+
+        _roof.gameObject.SetActive(true);
+    }
+}
