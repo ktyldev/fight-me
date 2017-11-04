9b7ffda4f2d1e77bbf8d495ef2709b6e6fb6c3c5
Cat Flynn <mewtwo97@hotmail.com>
2017-11-04 16:49:19 +0000

    Make individual object prefabs

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,30 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-
-    public Transform trackedObject;
+    
     public float lerpT;
     public float distance;
     public float angle;
     public Material postFXMaterial;
     public Shader[] drunkShaders;
-    public BloodAlcohol playerBAC;
+
+    private Transform _trackedObject;
+    private BloodAlcohol _bac;
+
+    void Start() {
+        var player = GameObject.FindGameObjectWithTag(GameTags.Player);
+
+        _trackedObject = player.transform;
+        _bac = player.GetComponent<BloodAlcohol>();
+    }
 
     void OnRenderImage (RenderTexture src, RenderTexture dest) {
         Graphics.Blit(src, dest, postFXMaterial);
     }
 
     void Update() {
-        postFXMaterial.shader = drunkShaders[(int)(Mathf.Floor(playerBAC.Current * 10))];
+        postFXMaterial.shader = drunkShaders[(int)(Mathf.Floor(_bac.Current * 10))];
     }
 
     void LateUpdate() {
-        if (trackedObject == null)
+        if (_trackedObject == null)
             return;
 
         var rotation = Quaternion.Euler(angle, 0, 0);
-        var targetPosition = trackedObject.transform.position + rotation * new Vector3(0, 0, -distance);
+        var targetPosition = _trackedObject.transform.position + rotation * new Vector3(0, 0, -distance);
         transform.position = Vector3.Lerp(transform.position, targetPosition, lerpT);
-        transform.LookAt(trackedObject);
+        transform.LookAt(_trackedObject);
     }
 }
