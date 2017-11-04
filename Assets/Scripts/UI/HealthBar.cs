bfd7e65d7dacc93805c6a7278fa092732c8c446c
Cat Flynn <mewtwo97@hotmail.com>
2017-11-04 21:40:47 +0000

    Start building level

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour {
-
+    
     public GameObject image;
     public GameObject healthObject;
+    public bool showAtStart;
 
     private Image _image;
     private Health _health;
     
     void Start() {
         _image = image.GetComponent<Image>();
-        
-        _image.transform.parent.gameObject.SetActive(false);
+
+        if (!showAtStart) {
+            _image.transform.parent.gameObject.SetActive(false);
+        }
 
         if (healthObject != null) {
             _health = healthObject.GetComponent<Health>();
