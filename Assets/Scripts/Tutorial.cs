c52261f3d68115754fa3b437f38f8ad5d0780dc5
Daniel Coady <daniel.coady@outlook.com.au>
2017-11-05 17:39:31 +1100

    Made boss thicc, added tutorial text

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour {
+    bool fading;
+	Image bg;
+	Text tutText;
+
+	void Start() {
+        bg = GetComponent<Image>();
+		tutText = gameObject.GetComponentInChildren<Text>();
+		StartCoroutine(TutorialFadeDelay());
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (fading) {
+            bg.color = Color.Lerp(bg.color, new Vector4(bg.color.r, bg.color.g, bg.color.b, 0), 0.1f);
+            tutText.color = Color.Lerp(tutText.color, new Vector4(tutText.color.r, tutText.color.g, tutText.color.b, 0), 0.1f);
+		}
+	}
+
+	IEnumerator TutorialFadeDelay() {
+		yield return new WaitForSeconds(5.0f);
+		fading = true;
+	}
+}
